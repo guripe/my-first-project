8a0092888e71d2f2e3841b8786cf51f2e61d224d
Gustavo Ribeiro <guripe123@gmail.com>
2022-05-03 21:56:28 -0300

    first commit

---
+++ controleDeGastos/control.js
@@ -0,0 +1,33 @@
+function tableRender(){
+    const $tbody = document.querySelector('#table tbody')
+
+    let content = ""
+
+    for (let index = 0; index < list.length; index++){
+        const item = list[index]
+        content += `
+            <tr>
+                <td>${item.description}</td>
+                <td>${item.price}</td>
+                <td> xxxx </td>
+                <td>${item.dueDate}</td>
+            </tr>
+        `
+    }
+    
+    $tbody.innerHTML=content;
+}
+
+let list = []
+
+function button(){
+    const description = document.querySelector("[name='description']").value
+    const price = document.querySelector("[name='price']").value
+    const dueDate = document.querySelector('[name="dueDate"]').value
+    list.push({description,price,dueDate})
+    tableRender()
+    localStorage.setItem("content", JSON.stringify(list))
+}
+
+list = JSON.parse(localStorage.getItem("content")) || []
+tableRender()
